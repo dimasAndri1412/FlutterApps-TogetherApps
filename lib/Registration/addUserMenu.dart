0b963b7a698d555966f8ce9fa22a1d91083a508f
dimasAndri1412 <168810210+dimasAndri1412@users.noreply.github.com>
2024-05-15 19:17:36 +0700

    penambahan nomer telpon, username logic untuk auto clear atau reset ada kesalahan

--- lib/Registration/addUserMenu.dart
+++ lib/Registration/addUserMenu.dart
@@ -11,109 +11,63 @@ class addUserMenu extends StatelessWidget{
   @override
   Widget build(BuildContext context){
 
-    return WillPopScope (
-        onWillPop: () async {
-          bool shouldPop = await _showExitConfirm(context);
-          return shouldPop;
-        },
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 98, 171, 232),
-          iconTheme: IconThemeData(color: Colors.white),
-          leading: GestureDetector(
-            child: Icon(Icons.arrow_back_ios, color: Colors.white,),
-            onTap: () {
-              _showExitConfirm(context);
-            },
-          ),
-          title: Text("PERSONAL INFORMATION",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 25),
-          ),
+    return Scaffold(
+      appBar : AppBar(
+        backgroundColor: Color.fromARGB(255, 98, 171, 232),
+        iconTheme: IconThemeData(color: Colors.white),
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back_ios,color: Colors.white,),
+          onTap: () {
+            Navigator.pop(context);
+            FullNameController.clear();
+            UserNameController.clear();
+            BirthDateController.clear();
+            AddressController.clear();
+            RolesController.clear();
+            PhoneNumbersController.clear();
+            EmailController.clear();
+            PassController.clear();
+            ConfPassController.clear();
+          },
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-              Color.fromARGB(255, 145, 201, 247),
-              Color.fromARGB(255, 98, 171, 232),
-              Color.fromARGB(255, 123, 185, 235),
-              Color.fromARGB(255, 255, 255, 255),
-            ]),
-          ),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 10,
-              ),
-              addUserMenu(),
-              Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only (
-                        topLeft: Radius.circular(60),
-                        topRight: Radius.circular(60),
-                      )
-                    ),
-                    child: SingleChildScrollView(
-                      child: addUserWrapper(),
-                    ),
-                  ),
-              )
-            ],
+        title: Text("PERSONAL INFORMATION",
+          style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontWeight: FontWeight.bold
           ),
         ),
       ),
-    );
-  }
-  Future<bool> _showExitConfirm(BuildContext context) async {
-    bool shouldPop = false;
-    await Get.dialog(
-      AlertDialog(
-        title: Text("Confirmation",
-          style: TextStyle(
-              color: Colors.blueAccent,
-              fontSize: 20,
-              fontWeight: FontWeight.bold),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter,colors: [
+            Color.fromARGB(255, 145, 201, 247),
+            Color.fromARGB(255, 98, 171, 232),
+            Color.fromARGB(255, 123, 185, 235),
+            Color.fromARGB(255, 255, 255, 255),
+          ]),
         ),
-        actions: <Widget>[
-          TextButton(
-              onPressed: () {
-                shouldPop = false;
-                Get.back();
-              },
-              child: Text("NO",
-                style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontWeight: FontWeight.bold),
-              )
-          ),
-          TextButton(
-              onPressed: () {
-                Get.back();
-                FullNameController.clear();
-                UserNameController.clear();
-                BirthDateController.clear();
-                AddressController.clear();
-                RolesController.clear();
-                PhoneNumbersController.clear();
-                EmailController.clear();
-                PassController.clear();
-                ConfPassController.clear();
-                Get.back();
-              },
-              child: Text("YES",
-                style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontWeight: FontWeight.bold),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 10,),
+            addUserHeader(),
+            Expanded(child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  )
               ),
-          ),
-        ],
-      )
+              child: SingleChildScrollView(
+                child: addUserWrapper(),
+              ),
+            ))
+          ],
+        ),
+      ),
     );
-    return shouldPop;
   }
 }
 
