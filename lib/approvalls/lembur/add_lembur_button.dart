63b0dce7719aec01b6ee7ef1177133d68a902bc2
Rogape <euclerr160@gmail.com>
2024-05-16 18:51:10 +0700

    Add menu request lembur user

---
+++ lib/approvalls/lembur/add_lembur_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class addLemburButton extends StatefulWidget {
+  const addLemburButton({super.key});
+
+  @override
+  State<addLemburButton> createState() => _addLemburButtonState();
+}
+
+class _addLemburButtonState extends State<addLemburButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MaterialButton(
+          child: Container(
+            height: 40,
+            width: 80,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 124, 185, 236),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text("Submit"),
+            ),
+          ),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Close"),
+                        )
+                      ],
+                      title: Text("Your Request Accepted"),
+                    ));
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/approvalls/lembur/pengajuan_lembur.dart
+++ lib/approvalls/lembur/pengajuan_lembur.dart
@@ -1,5 +1,8 @@
+import 'package:absent_project/approvalls/lembur/add_lembur_button.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class PengajuanLembur extends StatefulWidget {
   const PengajuanLembur({super.key});
@@ -10,6 +13,7 @@ class PengajuanLembur extends StatefulWidget {
 
 class _PengajuanLemburState extends State<PengajuanLembur> {
   final reasonLemburController = TextEditingController();
+  final _dateLemburController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +30,7 @@ class _PengajuanLemburState extends State<PengajuanLembur> {
         ),
         elevation: 0,
         title: Text(
-          "Form Pengajuan Lembur",
+          "Overtime Application Form",
           style: TextStyle(
               fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
         ),
@@ -36,10 +40,10 @@ class _PengajuanLemburState extends State<PengajuanLembur> {
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 20),
+                padding: const EdgeInsets.only(top: 160, left: 30, right: 30),
                 child: Container(
                   width: 390,
-                  height: 600,
+                  height: 300,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20),
@@ -50,35 +54,104 @@ class _PengajuanLemburState extends State<PengajuanLembur> {
                           offset: Offset(5, 5))
                     ],
                   ),
-                  child: Form(
-                      child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(color: Colors.black))),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                              labelText: "Reason of Overtime",
-                              hintText: "Please input you reason here",
-                              hintStyle: TextStyle(color: Colors.grey),
-                              border: InputBorder.none),
-                          controller: reasonLemburController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your reason here';
-                            }
-                            return null;
-                          },
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 5, left: 15, right: 15),
+                    child: Form(
+                        child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(color: Colors.black))),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                labelText: "Reason of Overtime",
+                                hintText: "Please input you reason here",
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none),
+                            controller: reasonLemburController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your reason here';
+                              }
+                              return null;
+                            },
+                          ),
                         ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(color: Colors.black))),
-                      ),
-                    ],
-                  )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.black))),
+                            child: DropdownSearch<String>(
+                              popupProps: PopupProps.dialog(
+                                showSelectedItems: true,
+                                // disabledItemFn: (String s) => s.startsWith('I'),
+                              ),
+                              items: [
+                                "Shift I",
+                                "Shift II",
+                                "Shift III",
+                              ],
+                              dropdownDecoratorProps: DropDownDecoratorProps(
+                                dropdownSearchDecoration: InputDecoration(
+                                  labelText: "Your Shift",
+                                  hintText: "Choose your shift !",
+                                ),
+                              ),
+                              // onChanged: print,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your reason here';
+                                }
+                                return null;
+                              },
+                            )),
+                        SizedBox(height: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(color: Colors.black))),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                labelText: "Date Leave",
+                                hintText: "Please enter your date leave",
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none,
+                                prefixIcon: Icon(Icons.calendar_today)),
+                            readOnly: true,
+                            controller: _dateLemburController,
+                            onTap: () async {
+                              await showDatePicker(
+                                      context: context,
+                                      initialDate: DateTime.now(),
+                                      firstDate: DateTime(2000),
+                                      lastDate: DateTime(2100))
+                                  .then((selectedDate) {
+                                if (selectedDate != null) {
+                                  _dateLemburController.text =
+                                      DateFormat('yyyy-MM-dd')
+                                          .format(selectedDate);
+                                }
+                              });
+                            },
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Enter the date !';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        addLemburButton(),
+                      ],
+                    )),
+                  ),
                 ),
               )
             ],
