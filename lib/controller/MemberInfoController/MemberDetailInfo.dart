3f39155e7e393901e45cd6308a231abc954c1f95
avesxvx <43735742+avesxvx@users.noreply.github.com>
2024-05-29 13:56:02 +0700

    home, login page, approvals, userhome

---
+++ lib/controller/MemberInfoController/MemberDetailInfo.dart
@@ -0,0 +1,10 @@
+class MemberDetailInfo {
+  final String username;
+  final String fullname;
+  final String role;
+  final String grup;
+  final String email;
+  final String phoneNumber;
+  MemberDetailInfo(this.username, this.fullname, this.role, this.grup,
+      this.email, this.phoneNumber);
+}
---
+++ lib/controller/MemberInfoController/detailMember.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../Keys.dart';
+import 'MemberDetailInfo.dart';
+
+class detailMember {
+  Future<List<MemberDetailInfo>?> getUsers() async {
+    var url = Uri.parse("http://192.168.2.159/FlutterAPI/ReadMemberInfo.php");
+    var data = await http.post(url,
+      body: {"username": emailController.text});
+    var jsonData = json.decode(data.body);
+
+    List<MemberDetailInfo> users = [];
+
+    for (var u in jsonData) {
+      MemberDetailInfo user = MemberDetailInfo(u["USERNAME"], u["full_name"], u["ROLE"],
+          u["grup"], u["email_address"], u["phone_number"]);
+      users.add(user);
+    }
+    return users;
+  }
+}
