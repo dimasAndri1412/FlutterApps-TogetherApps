62d8b4a47ee15adc8af70cbabfde99b488e3c1ed
dimasAndri1412 <168810210+dimasAndri1412@users.noreply.github.com>
2024-12-03 15:05:41 +0700

    penambahan validasi untuk email dan username yang sudah terdaftar tidak bisa di gunakan lagi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
+import 'package:absent_project/controller/AbsentController/ClockInState.dart';
 import 'package:flutter/material.dart';
 import 'package:absent_project/login/LoginPage.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(
+  MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => ClockInState()),
+    ],
+    child: const GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginPage()
+      // gmapsClockOutPages(imagePath: 'assets/images/analysis.png',),
+    ),
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
